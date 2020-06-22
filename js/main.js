92d149572b9d28f2dae65b8612bd6a8c60ba893a
Bogdan <bogdankuzzmin@gmail.com>
2020-06-22 06:39:05 +0600

    created form.js, added form validation to this file

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@
 var PIN_NUMBERS = 8;
 
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var TYPE_TO_RU = {
+window.TYPE_TO_RU = {
   palace: 'Дворец',
   flat: 'Квартира',
   house: 'Дом',
   bungalo: 'Бунгало'
 };
-var ROOMS = ['1', '2', '3', '100'];
+window.ROOMS = ['1', '2', '3', '100'];
 var GUESTS = ['1', '2', '3'];
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
@@ -26,7 +26,7 @@ var Y_MAX = 630;
 var PRICE_MIN = 0;
 var PRICE_MAX = 1000000;
 
-var PRICES_FOR_TYPES = {
+window.PRICES_FOR_TYPES = {
   bungalo: 0,
   flat: 1000,
   house: 5000,
@@ -77,7 +77,7 @@ var getAdvertisements = function () {
         adress: locationX + ', ' + locationY,
         price: getRandomInteger(PRICE_MIN, PRICE_MAX),
         type: getRandomElement(TYPE),
-        rooms: getRandomElement(ROOMS),
+        rooms: getRandomElement(window.ROOMS),
         guests: getRandomElement(GUESTS),
         checkin: getRandomElement(CHECKIN),
         checkout: getRandomElement(CHECKOUT),
@@ -181,7 +181,7 @@ var addCard = function (mapPinId) {
     cardTitle.textContent = adv.offer.title;
     cardAdress.textContent = adv.offer.adress;
     cardPrice.textContent = adv.offer.price + '₽/ночь';
-    cardType.textContent = TYPE_TO_RU[adv.offer.type];
+    cardType.textContent = window.TYPE_TO_RU[adv.offer.type];
     cardRoomAndGuests.textContent = adv.offer.rooms + ' комнаты для ' + adv.offer.guests + ' гостей';
     cardCheckinAndCheckout.textContent = 'Заезд после ' + adv.offer.checkin + ', выезд до ' + adv.offer.checkout;
     cardFeatures.appendChild(renderCardFeatures(adv.offer.features, cardFeatures));
@@ -230,61 +230,6 @@ var mapPinMainActiveHandler = function (evt) {
   }
 };
 
-var inputInvalidHandler = function (element) {
-  if (element.validity.valueMissing) {
-    element.setCustomValidity('Обязательное поле');
-  } else if (element.validity.tooShort) {
-    element.setCustomValidity('Минимальное значение - ' + element.minLength + ' симв.');
-  } else if (element.validity.tooLong) {
-    element.setCustomValidity('Максимальное значение - ' + element.maxLength + ' симв.');
-  } else if (element.validity.rangeUnderflow) {
-    element.setCustomValidity('Минимальная цена состоявляет - ' + element.min + ' руб.');
-  } else if (element.validity.rangeOverflow) {
-    element.setCustomValidity('Максимальная цена состоявляет - ' + element.max + ' руб.');
-  } else {
-    element.setCustomValidity('');
-  }
-};
-
-var inputTitleInputHandler = function () {
-  var valueLength = inputTitle.value.length;
-
-  inputTitle.reportValidity();
-
-  if (valueLength < inputTitle.minLength) {
-    inputTitle.setCustomValidity('Нужно ввести еще ' + (inputTitle.minLength - valueLength) + ' симв.');
-  } else if (valueLength > inputTitle.maxLength) {
-    inputTitle.setCustomValidity('Нужно удалить лишние ' + (valueLength - inputTitle.maxLength) + ' симв.');
-  } else {
-    inputTitle.setCustomValidity('');
-  }
-};
-
-var inputTypeChangeHandler = function () {
-  inputPrice.setAttribute('min', PRICES_FOR_TYPES[inputType.value]);
-  inputPrice.setAttribute('placeholder', PRICES_FOR_TYPES[inputType.value]);
-};
-
-var inputTimeInOutChangeHandler = function () {
-  if (inputTimeIn.options.selectedIndex !== inputTimeOut.options.selectedIndex) {
-    inputTimeOut.setCustomValidity('Время выезда должно совподать с временем заезда');
-  } else {
-    inputTimeOut.setCustomValidity('');
-  }
-};
-
-var inputGuestsRoomsChangeHandler = function () {
-  if (inputGuestNumber.value === '0' && inputRoomNumber.value !== ROOMS[3]) {
-    inputGuestNumber.setCustomValidity('Для ' + '"' + inputGuestNumber.options[3].label + '"' + ' допустимое значение: ' + '"' + inputRoomNumber.options[3].label + '"');
-  } else if (inputRoomNumber.value === ROOMS[3] && inputGuestNumber.value !== '0') {
-    inputGuestNumber.setCustomValidity('Для ' + '"' + inputRoomNumber.options[3].label + '"' + ' допустимое значение: ' + '"' + inputGuestNumber.options[3].label + '"');
-  } else if (inputGuestNumber.value > inputRoomNumber.value) {
-    inputGuestNumber.setCustomValidity('Количество гостей не должно привышать количество комнат');
-  } else {
-    inputGuestNumber.setCustomValidity('');
-  }
-};
-
 var mapCardOpenHandler = function (evt) {
   var mapPin = evt.target.closest('.map__pin:not(.map__pin--main)');
   var mapCard = document.querySelector('.map__card');
@@ -319,18 +264,6 @@ var mapCardCloseHandler = function (evt) {
 var init = function () {
   mapPinMain.addEventListener('mousedown', mapPinMainActiveHandler);
   mapPinMain.addEventListener('keydown', mapPinMainActiveHandler);
-  inputTitle.addEventListener('invalid', function () {
-    inputInvalidHandler(inputTitle);
-  });
-  inputPrice.addEventListener('invalid', function () {
-    inputInvalidHandler(inputPrice);
-  });
-  inputTitle.addEventListener('input', inputTitleInputHandler);
-  inputType.addEventListener('input', inputTypeChangeHandler);
-  inputTimeIn.addEventListener('input', inputTimeInOutChangeHandler);
-  inputTimeOut.addEventListener('input', inputTimeInOutChangeHandler);
-  inputGuestNumber.addEventListener('input', inputGuestsRoomsChangeHandler);
-  inputRoomNumber.addEventListener('input', inputGuestsRoomsChangeHandler);
 };
 
 var map = document.querySelector('.map');
@@ -339,17 +272,8 @@ var mapPins = document.querySelector('.map__pins');
 var adForm = document.querySelector('.ad-form');
 var adFormFieldsets = adForm.querySelectorAll('fieldset');
 
-var inputTitle = document.querySelector('#title');
-var inputPrice = document.querySelector('#price');
-var inputType = document.querySelector('#type');
-var inputTimeIn = document.querySelector('#timein');
-var inputTimeOut = document.querySelector('#timeout');
-var inputRoomNumber = document.querySelector('#room_number');
-var inputGuestNumber = document.querySelector('#capacity');
-
 var advertisementsArr = getAdvertisements();
 
 toggleElements(adFormFieldsets, true);
-inputGuestsRoomsChangeHandler();
 setValueAddressInput(MAP_PIN_MAIN_HEIGHT / 2);
 init();
